024b31ace333e5059f2f2dbb11ada9c601756e1a
GodOfKings <developers.castle.1@gmail.com>
2013-08-17 20:49:40 +0530

    Added New Method.. Say Bye...

--- HealthyNature/src/com/home/app/container/ProductDetails.java
+++ HealthyNature/src/com/home/app/container/ProductDetails.java
@@ -4,4 +4,8 @@ public class ProductDetails {
 	public static String sayHello(final String name){
 		return "Hello "+name+" !.. Welcome to our Company..";
 	}
+	
+	public static String sayBye(){
+		return "Bye.. Have a nice day.. :-)";
+	}
 }
